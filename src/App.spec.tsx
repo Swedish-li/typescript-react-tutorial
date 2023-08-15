ec97b125c3f56a2e5736f6bdc205f68a72d36471
Swedish-li <one.piece.li.2016@gmail.com>
2023-08-15 13:40:22 +0000

    fix lint error

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { createRenderer }  from 'react-test-renderer/shallow'
+import { createRenderer } from 'react-test-renderer/shallow'
 import App from './App'
 import Header from './containers/Header'
 import MainSection from './containers/MainSection'
